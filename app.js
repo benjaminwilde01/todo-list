907f76c95a7574b189a1fd56411d16e6d7d08264
Benjamin Wilde <bendavidwilde95@gmail.com>
2021-08-09 11:28:24 -0500

    render list function created

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log('hi')
+// Declare Variables 
+const submitButton = document.querySelector('#submitBtn')
+const todoContainer = document.querySelector('.todo')
+
+const todoArray = []
+
+// Displays todo items
+const renderList = () => {
+    let listItemDiv = document.createElement('div')
+    let createBtn = document.createElement('button')
+    createBtn.innerText = 'Completed'
+    createBtn.setAttribute('id', 'lists')
+
+    todoArray.forEach((item) => {
+        listItemDiv.innerText = item
+        listItemDiv.className = 'todo-item'
+        todoContainer.append(listItemDiv)
+        listItemDiv.append(createBtn)
+    })
+}
