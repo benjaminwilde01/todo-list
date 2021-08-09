8772241263da027dc3e494f7ce6ecf2de3070944
Benjamin Wilde <bendavidwilde95@gmail.com>
2021-08-09 11:49:28 -0500

    todo items can be moved to the completed list now

--- app.js
+++ app.js
@@ -7,16 +7,33 @@ const todoArray = []
 // Displays todo items
 const renderList = () => {
     let listItemDiv = document.createElement('div')
-    let createBtn = document.createElement('button')
-    createBtn.innerText = 'Completed'
-    createBtn.setAttribute('id', 'lists')
+    let completeBtn = document.createElement('button')
+    completeBtn.innerText = 'Completed'
+    completeBtn.setAttribute('id', 'lists')
 
     todoArray.forEach((item) => {
         listItemDiv.innerText = item
         listItemDiv.className = 'todo-item'
         todoContainer.append(listItemDiv)
-        listItemDiv.append(createBtn)
+        listItemDiv.append(completeBtn)
     })
+
+    completeBtn.onclick = () => {
+        const completedDiv = document.querySelector('.completed')
+        completedDiv.append(listItemDiv)
+        listItemDiv.className = 'done-item'
+        completeBtn.remove()
+        
+        const removeBtn = document.createElement('button')
+        removeBtn.innerText = 'Remove'
+
+        listItemDiv.append(removeBtn)
+        removeBtn.onclick = () => {
+            listItemDiv.remove()
+        }
+    }
+
+
 }
 
 
