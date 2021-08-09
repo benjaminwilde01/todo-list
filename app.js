640fd17e621505bb5d65511f29299767656c21aa
Benjamin Wilde <bendavidwilde95@gmail.com>
2021-08-09 13:55:52 -0500

    moving variables and createElement broke the app. Returned them to their original places

--- app.js
+++ app.js
@@ -4,15 +4,14 @@ const todoContainer = document.querySelector('.todo')
 const completedDiv = document.querySelector('.completed')
 
 // Create Elements
-const removeBtn = document.createElement('button')
-let listItemDiv = document.createElement('div')
-let completeBtn = document.createElement('button')
 
 const todoArray = []
 
 // Displays todo items
 const renderList = () => {
+    const listItemDiv = document.createElement('div')
     // Creates 'completed' Button
+    const completeBtn = document.createElement('button')
     completeBtn.innerText = 'Completed'
     completeBtn.setAttribute('id', 'lists')
 
@@ -28,9 +27,12 @@ const renderList = () => {
         completedDiv.append(listItemDiv)
         listItemDiv.className = 'done-item'
         completeBtn.remove()
-    
+        
+        const removeBtn = document.createElement('button')
+
         removeBtn.innerText = 'Remove'
         listItemDiv.append(removeBtn)
+
         removeBtn.onclick = () => {
         listItemDiv.remove()
         }
@@ -41,6 +43,6 @@ const renderList = () => {
 submitButton.onclick = () => {
     const inputValue = document.querySelector('#input-box').value
     todoArray.push(inputValue)
-    console.log(todoArray)
+    document.querySelector('#input-box').value = ''
     renderList()
 }
